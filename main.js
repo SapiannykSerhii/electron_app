ba7ca2a4fad89a781bd2ff60c4de0730fce8541b
Serhii Sapiannyk <serhii09@icloud.com>
2022-05-15 19:07:36 +0200

    Add functinalities to app

--- main.js
+++ main.js
@@ -8,14 +8,16 @@ function main() {
   // create new window
   let mainWindow = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      frame: false
     },
-    width: 800,
-    height: 600,
+    width: 520,
+    height: 650,
   })
 
   // load app/index.html as the window content
   mainWindow.loadFile(path.join('app', 'index.html'));
+  // mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', main);
